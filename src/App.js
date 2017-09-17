497bf65db9e9cfef6b2e89dd166e07d631f7f8d3
michalzmujdzin <yokomura123@gmail.com>
2017-09-17 11:35:57 +0200

    check2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function App2(props) {
+    return <button onClick={props.func}> {props.name}</button>
 }
 
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0,
+            lista: ["a", "b", "c"]
+        }
+    }
+
+    klikam = () => {
+        this.setState({counter: this.state.counter + 1},
+            () => {
+                console.log(this.state.counter);
+            })
+
+    }
+
+    render() {
+        console.log(this.props);
+        const name = "Ania";
+        let ul = this.state.lista.map((e) => {
+            return <li key={e}>{e}</li>
+        });
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    Hi {this.props.name}, {this.state.counter}
+                </p>
+                <button onClick={this.klikam}>
+                    tutaj
+                </button>
+                <App2 name={this.props.name} func={this.klikam}/>
+                <div>{this.state.lista}</div>
+                <ul>{ul}</ul>
+            </div>
+        );
+    }
+}
 
 export default App;
